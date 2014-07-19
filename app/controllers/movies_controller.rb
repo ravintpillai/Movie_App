a1dbdd3924dda3c30cf01c094f0e9d57a57e33c6
tpillairavin <ravin@gocardless.com>
2014-07-19 10:14:27 +0100

    making movies shit work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,38 @@
 class MoviesController < ApplicationController
+	before_action :load_movie, only: [:edit, :show, :update]
 
 	def index
 		@movies = Movie.all
 	end
+
+	def show
+	end
+
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		@movie = Moive.new(safe_movie_params)
+		if @shirt.save
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+
+	private
+		def load_movie
+			@movie = Movie.find_by_id(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			Flash.now[:notice] = "Invalid Movie Id #{params[:id]}"
+			redirect_to root_path
+		end
+
+		def safe_movie_params
+			params.require('movie').permit(:title,:description,:release_year)
+		end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ActiveRecord::Base
+	validates :title, :description, :release_year, presence: true
 end
